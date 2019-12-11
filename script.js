d55fe261767bdc857d9a8886106c2e2184b0bfa8
Huy Nguyen <giahuy2201@icloud.com>
2019-12-10 23:39:07 -0500

    refactor toward OOP

--- script.js
+++ script.js
@@ -1,169 +1,219 @@
 let randomWords = require('random-words');
 import 'bootstrap';
-import 'jquery';
-import _ from 'lodash';
 
 // -------------------------------------
 // Setup
 // -------------------------------------
 
-let point = 0
-let position = 0
-let started = false
-let passage = ''
-let buffer = ''
-let sampleTop = document.querySelector('.top')
-let sampleBot = document.querySelector('.bot')
-updatePassage()
-let input = document.querySelector('.word-input')
-input.addEventListener('input', function () {
-    // document.querySelector('.heading').innerHTML+='*'
+let rightWords = 0
+let timer = new Timer(document.querySelector('#timer'), 60, displayWPM);
+let wordInput = new WordInput(document.querySelector('.word-input'));
+let wordBox = new WordBox(document.querySelector('.top'),document.querySelector('.bot'));
+wordBox.update();
+wordInput.listen(keystroke);
 
-    if (!started) {
-        started = true
-        countdown(60, calculateWPM)
-    }
-    if (input.value.includes(' ')) {
-        let word = getWord(position)
-        let inputText = popInput()
-        let sampleText = word.innerHTML
-        if (inputText == sampleText) {
-            word.classList.add('right')
-            point += 1
+// -------------------------------------
+// Input listener
+// -------------------------------------
+
+function keystroke() {
+    // Start timer
+    timer.start()
+    // Check input
+    let word = wordBox.get();
+    if (wordInput.hasNewWord()) {
+        // Compare the words
+        if (wordInput.pop() == word.text()) {
+            word.addClass('right')
+            rightWords++;
         } else {
-            // prevent space spam
-            if (inputText == '') {
-                return
-            } else {
-                word.classList.remove('mistake')
-                word.classList.add('wrong')
-            }
+            word.removeClass('mistake')
+            word.addClass('wrong')
         }
         // highlight the next word instead
-        word.classList.remove('highlight')
-        position += 1
-        let nextWord = getWord(position)
-        if (!nextWord) {
-            // update passage
-            updatePassage()
-            nextWord = getWord(position)
-        }
-        nextWord.classList.add('highlight')
-        // updatePoint()
+        word.removeClass('highlight');
+        wordBox.next().addClass('highlight');
+
     } else {
-        let word = getWord(position)
-        word.classList.remove('highlight')
-        word.classList.add('highlight')
-        let inputText = peekInput()
-        let sampleText = word.innerHTML
-        if (goodInput(sampleText, inputText)) {
-            word.classList.remove('mistake')
+        // check if input is part of the sample word
+        if (word.text().indexOf(wordInput.peek()) == 0) {
+            word.removeClass('mistake')
         } else {
-            word.classList.remove('mistake')
-            word.classList.add('mistake')
+            word.addClass('mistake')
         }
+        // mark it
+        word.addClass('highlight')
     }
-})
-
-// -------------------------------------
-// Functional
-// -------------------------------------
-
-function getWord(position) {
-    return document.querySelector('.top > .word:nth-child(' + (position + 1) + ')');
-}
-function goodInput(sample, input) {
-    return sample.indexOf(input) == 0
-}
-function popInput() {
-    let text = input.value.trim()
-    input.value = ''
-    return text
-}
-function peekInput() {
-    return input.value
 }
 
-function countdown(seconds, done) {
-    let timer = setInterval(function () {
-        if (seconds == 0) {
-            clearInterval(timer)
-            done()
-        } else {
-            seconds--;
-            updateTimer(seconds);
-        }
-    }, 1000);
-}
-
-
-function calculateWPM() {
-    let p = document.querySelector('#point')
-    p.innerHTML = point + ' wpm'
+function displayWPM() {
+    let wpm = document.querySelector('#point')
+    wpm.innerHTML = rightWords + ' wpm'
 }
 
 // -------------------------------------
-// Update UI
+// Word model
 // -------------------------------------
 
-function updatePassage() {
-    if (buffer == '') {
-        buffer = randomWords(10).join(' ')
+function Word(document) {
+    this.document = document;
+
+    this.text = function () {
+        return this.document.innerHTML;
     }
-    passage = buffer
-    buffer = randomWords(10).join(' ')
-    position = 0
-    fit2lines()
-    updateSample()
+    this.addClass = function (className) {
+        this.document.classList.remove(className);
+        this.document.classList.add(className);
+    }
+    this.removeClass = function (className) {
+        this.document.classList.remove(className);
+    }
 }
 
-function fit2lines() {
-    updateSample()
-    // check sampleTop
-    let topWords = document.querySelectorAll('.top > .word')
-    let originWord = topWords[0]
-    for (let i = 0; i < topWords.length; i++) {
-        const word = topWords[i];
-        if (word.offsetTop > originWord.offsetTop) {
-            // console.log('top ' + i)
-            let list = passage.split(' ')
-            passage = list.splice(0, i).join(' ')
-            buffer = list.join(' ') + ' ' + buffer
-            break
-        }
+// -------------------------------------
+// WordBox model
+// -------------------------------------
+
+function WordBox(topDocument, botDocument) {
+    this.topLine = topDocument;
+    this.botLine = botDocument;
+    this.topText = '';
+    this.botText = '';
+    this.iterator = 0;
+
+    // Get current word
+    this.get = function () {
+        return new Word(this.topLine.children[this.iterator])
     }
-    // update to get the real change
-    updateSample()
-    // check sampleBot
-    let botWords = document.querySelectorAll('.bot > .word')
-    originWord = botWords[0]
-    for (let i = 0; i < botWords.length; i++) {
-        const word = botWords[i];
-        if (word.offsetTop > originWord.offsetTop) {
-            // console.log('bot ' + i)
-            let list = buffer.split(' ')
-            buffer = list.splice(0, i).join(' ')
-            break
+    // Get the next word
+    this.next = function () {
+        this.iterator++;
+        // No more word -> update
+        if (this.topLine.childElementCount < this.iterator + 1) {
+            this.update();
+        }
+        return new Word(this.topLine.children[this.iterator])
+    }
+    this.update = function () {
+        if (this.botText == '') {
+            this.botText = randomWords(10).join(' ')
+        }
+        this.topText = this.botText;
+        this.botText = randomWords(10).join(' ')
+        this.iterator = 0;
+        // Fit words into 2 lines
+        this.calibrate();
+        this.render();
+    }
+    // Display the wordbox
+    this.render = function () {
+        // Construct the top line
+        let topList = this.topText.split(' ');
+        let formatedTopList = [];
+        topList.forEach(word => {
+            formatedTopList.push('<span class="word">' + word + '</span>')
+        });
+        this.topLine.innerHTML = formatedTopList.join(' ')
+        // Construct the bottom line
+        let botList = this.botText.split(' ');
+        let formatedBotList = [];
+        botList.forEach(word => {
+            formatedBotList.push('<span class="word">' + word + '</span>')
+        });
+        this.botLine.innerHTML = formatedBotList.join(' ')
+    }
+    // Fit words into lines
+    this.calibrate = function () {
+        // Display to get the overflow attribute - offsetTop
+        this.render();
+        // check top line
+        let topWords = this.topLine.children;
+        let originWord = topWords[0]
+        for (let i = 0; i < topWords.length; i++) {
+            const word = topWords[i];
+            // find the breakpoint
+            if (word.offsetTop > originWord.offsetTop) {
+                let list = this.topText.split(' ')
+                this.topText = list.splice(0, i).join(' ')
+                this.botText = list.join(' ') + ' ' + this.botText
+                break
+            }
+        }
+        // update to get the real change
+        this.render();
+        // check bottom line
+        let botWords = this.botLine.children;
+        originWord = botWords[0]
+        for (let i = 0; i < botWords.length; i++) {
+            const word = botWords[i];
+            if (word.offsetTop > originWord.offsetTop) {
+                let list = this.botText.split(' ')
+                this.botText = list.splice(0, i).join(' ')
+                break
+            }
         }
     }
 }
-function updateSample() {
-    let listTop = passage.split(' ')
-    let formatedListTop = []
-    listTop.forEach(word => {
-        formatedListTop.push('<span class="word">' + word + '</span>')
-    });
-    sampleTop.innerHTML = formatedListTop.join(' ')
-    let listBot = buffer.split(' ')
-    let formatedListBot = []
-    listBot.forEach(word => {
-        formatedListBot.push('<span class="word">' + word + '</span>')
-    });
-    sampleBot.innerHTML = formatedListBot.join(' ');
+
+// -------------------------------------
+// WordInput
+// -------------------------------------
+
+function WordInput(document) {
+    this.document = document;
+
+    this.peek = function () {
+        return this.document.value.trim();
+    }
+    this.pop = function () {
+        let text = this.document.value.trim();
+        this.document.value = ''
+        return text
+    }
+    this.hasNewWord = function () {
+        if (this.document.value.includes(' ')) {
+            // Prevent space spam
+            if (this.document.value.trim() != '') {
+                return true;
+            }
+            this.document.value = '';
+        }
+        return false;
+    }
+    this.listen = function (callback) {
+        this.document.addEventListener('input', callback);
+    }
 }
-function updateTimer(seconds) {
-    let timer = document.querySelector('#timer');
-    let min = parseInt(seconds / 60);
-    let sec = seconds % 60;
-    timer.innerHTML = min + ':' + sec;
+
+// -------------------------------------
+// Timer
+// -------------------------------------
+
+function Timer(document, duration, callback) {
+    this.duration = duration;
+    this.document = document;
+    this.callback = callback;
+    this.started = false;
+
+    this.start = function () {
+        if (this.started) {
+            return
+        }
+        this.started = true;
+        let timer = this;
+        let countdown = setInterval(function () {
+            if (timer.duration == 0) {
+                clearInterval(countdown);
+                callback();
+            } else {
+                timer.duration--;
+                timer.update();
+            }
+        }, 1000);
+    }
+    this.update = function () {
+        let min = parseInt(this.duration / 60);
+        let sec = this.duration % 60;
+        this.document.innerHTML = min + ':' + sec;
+    }
 }
